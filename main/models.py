08e9768f306b2c0b84181f6eb0b1593160bb8c1b
namarabigail <bigelnp10@gmail.com>
2025-09-17 16:00:45 +0700

    Tugas Individu 4

--- main/models.py
+++ main/models.py
@@ -1,5 +1,6 @@
 import uuid
 from django.db import models
+from django.contrib.auth.models import User
 
 class Product(models.Model): 
     CATEGORY_CHOICES = [
@@ -10,6 +11,7 @@ class Product(models.Model):
         ('merchandise', 'Merchandise'),
     ]  
     
+    user = models.ForeignKey(User, on_delete=models.CASCADE, null=True)
     name = models.CharField(max_length=255)
     price = models.PositiveIntegerField(default=0)
     description = models.TextField()
