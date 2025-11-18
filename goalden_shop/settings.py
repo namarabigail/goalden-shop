fd412cb3a833e1fa1f3fba52126b17aa61422fb0
namarabigail <bigelnp10@gmail.com>
2025-11-19 04:25:35 +0700

    Tutorial 8 selesai

--- goalden_shop/settings.py
+++ goalden_shop/settings.py
@@ -33,7 +33,7 @@ CSRF_TRUSTED_ORIGINS = [
     "https://abigail-namaratonggi-goaldenshop.pbp.cs.ui.ac.id"
 ]
 
-ALLOWED_HOSTS = ["localhost", "127.0.0.1","abigail-namaratonggi-goaldenshop.pbp.cs.ui.ac.id"]
+ALLOWED_HOSTS = ["localhost", "127.0.0.1","abigail-namaratonggi-goaldenshop.pbp.cs.ui.ac.id","10.0.2.2"]
 
 
 # Application definition
@@ -45,7 +45,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'main'
+    'main',
+    'authentication',
+    'corsheaders'
 ]
 
 MIDDLEWARE = [
@@ -57,6 +59,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'corsheaders.middleware.CorsMiddleware'
 ]
 
 ROOT_URLCONF = 'goalden_shop.urls'
@@ -154,3 +157,9 @@ else:
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+CORS_ALLOW_ALL_ORIGINS = True
+CORS_ALLOW_CREDENTIALS = True
+CSRF_COOKIE_SECURE = True
+SESSION_COOKIE_SECURE = True
+CSRF_COOKIE_SAMESITE = 'None'
+SESSION_COOKIE_SAMESITE = 'None'
--- main/urls.py
+++ main/urls.py
@@ -1,10 +1,10 @@
 from django.urls import path
-from main.views import show_main, add_products, show_products, show_xml, show_json, show_xml_by_id, show_json_by_id
+from main.views import show_main, add_products, show_products, show_xml, show_json, show_xml_by_id, show_json_by_id, proxy_image
 from main.views import register
 from main.views import login_user
 from main.views import logout_user
 from main.views import edit_products, delete_products
-from main.views import add_products_entry_ajax, delete_product_ajax, edit_product_ajax, register_ajax, login_ajax, logout_ajax
+from main.views import add_products_entry_ajax, delete_product_ajax, edit_product_ajax, register_ajax, login_ajax, logout_ajax, create_news_flutter
 
 app_name = 'main'
 
@@ -28,4 +28,6 @@ urlpatterns = [
     path('login-ajax/', login_ajax, name='login_ajax'),
     path('logout-ajax/', logout_ajax, name='logout_ajax'),
     path('delete-product-ajax/<str:id>/', delete_product_ajax, name='delete_product_ajax'),
+    path('proxy-image/', proxy_image, name='proxy_image'),
+    path('create-flutter/', create_news_flutter, name='create_news_flutter'),
 ]
